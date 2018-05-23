0c4c3ced098a5479f895c5707b43ba3e979b47fd
Albert Lopez <gun.rugger@gmail.com>
2018-05-23 15:15:42 -0700

    chmod 744 setup.sh and folded appendTo.bashrc into the setup.sh

--- appendTo.bashrc
+++
@@ -1,8 +0,0 @@
-
-#bash custom#
-if [ -f ~/.bash_custom/.bash_aliases ]; then
-    . ~/.bash_custom/.bash_aliases
-fi
-if [ -f ~/.bash_custom/.bash_functions ]; then
-    . ~/.bash_custom/.bash_functions
-fi
--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
-cat ~/.bash_custom/appendTo.bashrc >> ~/.bashrc
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+appendToBashRC="if [ -f $DIR/.bash_aliases ]; then . $DIR/.bash_aliases; fi; if [ -f $DIR/.bash_functions ]; then . $DIR/.bash_functions; fi;"
+grepAppend=$(echo $appendToBashRC | sed "s/\[/\\\[/g" | sed "s/\]/\\\]/g")
+if [[ $(grep "$grepAppend" ~/.bashrc) ]]; then
+	echo "Bash custom has already be setup. try running 'source ~/.bashrc' or opening an new terminal session.";
+else
+	bashCustom="##Bash Custom##";
+	echo "Appending the following to your ~/.bashrc file";
+	echo $bashCustom;
+	echo $appendToBashRC;
+	echo $bashCustom >> ~/.bashrc;
+	echo $appendToBashRC >> ~/.bashrc;
+fi;
+source ~/.bashrc
